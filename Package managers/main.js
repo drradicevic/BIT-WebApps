120e833074df2a4f547f315f9d3f6ce8795fcba3
drradicevic <drradicevic@gmail.com>
2021-08-12 20:13:09 +0200

    Update

--- Package managers/main.js
+++ Package managers/main.js
@@ -1,6 +1,8 @@
 let figlet = require("figlet");
 let funnyWords = require("funny-words");
 let cowsay = require("cowsay");
+let oneLinerJoke = require("one-liner-joke");
+let randomWords = require("random-words");
 
 console.log(
   figlet.textSync("Hello World!", {
@@ -21,6 +23,7 @@ console.log(
     whitespaceBreak: true,
   })
 );
+//figlet
 
 console.log(funnyWords("Don't worry, be happy!"));
 // Output could be something "D'not wrory, be hppay!"
@@ -32,5 +35,16 @@ console.log(
     T: "U ",
   })
 );
-
 // or cowsay.think()
+
+const getRandomJoke = oneLinerJoke.getRandomJoke();
+console.log(getRandomJoke.body);
+
+// The variable getRandomJoke will contain a random joke
+
+console.log(randomWords());
+
+console.log(randomWords(5));
+
+console.log(randomWords({ min: 3, max: 10 }));
+//randomWords
